3dd8b01b1813a6e4478db4127a7e8b8dd353633a
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2024-01-30 14:33:04 +0530

    do new changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import Navbar from './components/Navbar';
 import Datascience from './pages/Datascience';
 import Home from './pages/Home';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import SignUp from './pages/SignUp/SignUp';
-import Login from './pages/Login/Login';
-import ForgotPwd from './pages/ForgotPwd/ForgotPwd';
-import ChangePassword from './components/ForgotPassword/ChangePassword';
+
 
 
 function App() {
@@ -17,10 +14,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
           <Route path="/Datascience" element={<Datascience/>} />
-          <Route path="/Signup" element={<SignUp></SignUp>}></Route>
-          <Route path="/Login" element={<Login></Login>} ></Route>
-          <Route path="/ForgotPassword" element={<ForgotPwd></ForgotPwd>}></Route>
-          <Route path="/ChangePassword" element={<ChangePassword></ChangePassword>}></Route>
+          
         </Routes>
         
       </BrowserRouter>
