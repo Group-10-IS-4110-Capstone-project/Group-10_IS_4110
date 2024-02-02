b54b5f4fdda924e725225ee1d5d5e3de2a3d4740
Pamudi Srikanthi <95670474+PamudiSrikanthi@users.noreply.github.com>
2024-02-02 10:43:12 +0530

    experts page new updates

--- src/pages/Experts.jsx
+++ src/pages/Experts.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar_2 from '../components/Navbar_2'
 import Footer from '../components/Footer'
 import Searchbar from '../components/Searchbar'
-import Page_change_btn from '../components/Page_change_btn'
 
 
 function Experts() {
@@ -12,7 +11,6 @@ function Experts() {
           <Navbar_2 />
       </div>
       <Searchbar />
-      <Page_change_btn />
       <Footer />
     </div>
   )
