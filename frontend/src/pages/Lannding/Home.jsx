dde3102490c67488d4001e09a5a54f094c1833d6
Sahan251 <sahanudayanga64@gmail.com>
2024-02-28 08:55:47 +0530

    landing updates

--- frontend/src/pages/Lannding/Home.jsx
+++ frontend/src/pages/Lannding/Home.jsx
@@ -3,6 +3,9 @@ import Hero from './sections/Hero'
 import "./Home.css"
 import About from './sections/About'
 import Explore from './sections/Explore'
+import Career from './sections/Career'
+import FAQSection from './sections/FAQSection'
+import Contact_us from './sections/Contact_us'
 
 const Home = () => {
   return (
@@ -10,8 +13,12 @@ const Home = () => {
       <Hero />
       <About />
       <Explore />
+      <Career/>
+      <FAQSection/>
+      <Contact_us/>
+
     </div>
   )
 }
 
-export default Home
+export default Home;
