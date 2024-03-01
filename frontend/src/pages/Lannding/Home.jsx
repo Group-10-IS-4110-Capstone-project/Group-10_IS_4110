f5316b66d1d77d90501d8be5bb80643ecf85bd2d
Sahan251 <sahanudayanga64@gmail.com>
2024-03-01 11:34:28 +0530

    Footer update

--- frontend/src/pages/Lannding/Home.jsx
+++ frontend/src/pages/Lannding/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from './sections/Hero'
-import NavBar from './sections/NavBar'
+
 import "./Home.css"
 
 import About from './sections/About'
@@ -8,6 +8,7 @@ import Explore from './sections/Explore'
 import Career from './sections/Career'
 import FAQSection from './sections/FAQSection'
 import Contact_us from './sections/Contact_us'
+import Footer from './sections/Footer'
 
 const Home = () => {
   return (
@@ -20,6 +21,7 @@ const Home = () => {
       <Career/>
       <FAQSection/>
       <Contact_us/>
+      <Footer/>
 
     </div>
   )
