124641539d4c2546121b1e5d956a330b84cb195b
VASPerera <avishka1999perera@gmail.com>
2024-03-07 01:07:22 +0530

    display expert

--- Backend/Routes/admin.js
+++ Backend/Routes/admin.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const Router = express.Router();
-const {adminRegister, searchUsers ,displayUsers, deleteUserById} = require("../Controllers/adminController");
+const {adminRegister, searchUsers ,displayUsers,displayExperts, deleteUserById} = require("../Controllers/adminController");
 const { verifyToken } = require("../middleware/protectRoute");
 
 Router.post("/register",adminRegister);
 Router.get('/search',verifyToken, searchUsers);
-Router.get('/displayusers',verifyToken, displayUsers);
+Router.get('/displayusers', displayUsers);
+Router.get('/displayExperts', displayExperts);
 Router.delete('/deleteuser/:id',verifyToken, deleteUserById);
 
 
--- frontend/src/pages/Admin/AdminEx.jsx
+++ frontend/src/pages/Admin/AdminEx.jsx
@@ -1,129 +1,140 @@
-import React from "react"; 
-import './AdminEx.css'
+import React, { useEffect, useState } from "react";
+import "./AdminEx.css";
 import Button from "react-bootstrap/Button";
 import white from "../../assets/white_background.jpg";
 
 function AdminEx() {
-    return (
+  const [experts, setExperts] = useState([]);
 
-<div className="admin-page">
+  useEffect(() => {
+    // Fetch data from the backend
+    fetch("http://localhost:3001/admin/displayExperts")
+      .then((response) => response.json())
+      .then((data) => setExperts(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
-    <div className="overlap-group-wrapper">
-
+  return (
+    <div className="admin-page">
+      <div className="overlap-group-wrapper">
         <div className="overlap-group">
+          <div className="group">
+            <div className="rectangle" />
 
-            <div className="group">
+            <div className="div" />
+          </div>
 
-                <div className="rectangle" />
+          <div className="under">
+            <a href="http://localhost:3000/AdminUnder">Undergraduate</a>
+          </div>
 
-                <div className="div" />
+          <div className="expert">
+            <a href="http://localhost:3000/AdminEx">Expert</a>
+          </div>
 
-            </div>
+          <div className="chat">Chat</div>
 
-                <div className="under">
-                <a href="http://localhost:3000/AdminUnder">
-                Undergraduate
-                  </a>
-                    
-                    </div>
+          <div className="Cont">Content</div>
 
+          <div className="text-wrapper-5">Expert</div>
 
-                <div className="expert">
-                <a href="http://localhost:3000/AdminEx">
-                Expert
-                  </a>
-                    
-                
-                </div>
+          <img className="line" alt="Line" src={white} />
 
-                <div className="chat">Chat</div>
+          <div className="rectangle-2" />
 
-                <div className="Cont">Content</div>
+          <div className="rectangle-3" />
 
-                <div className="text-wrapper-5">Expert</div>
+          <div className="rectangle-4" />
 
-                <img
+          <div className="rectangle-5" />
 
-                className="line"
+          <div className="rectangle-6" />
 
-                alt="Line"
+          <div className="rectangle-7" />
 
-                src={white}/>
+          {/* //*************** */}
 
-                <div className="rectangle-2" />
-
-                <div className="rectangle-3" />
-
-                <div className="rectangle-4" />
-
-                <div className="rectangle-5" />
-
-                <div className="rectangle-6" />
-
-                <div className="rectangle-7" />
-
-                <div className="text-wrapper-6">
-                <Button className="butn" variant="primary" >
-                Remove
-                </Button>{" "}
+          <div className="admin-box">
+            <div className="view-expert">
+              {experts.map((expert) => (
+                <div key={expert.id} className="expert-item">
+                  <p className="f-name">{expert.Name}</p>
+                  
+                    <Button className="remove-btn" variant="primary">
+                      Remove
+                    </Button>{" "}
         
-                    
-                    </div>
-
-                <div className="text-wrapper-7">
-               
-            
-                <Button className="butn" variant="primary" >
-                Remove
-                </Button>{" "}
-                    </div>
-
-                <div className="text-wrapper-8"> <Button className="butn" variant="primary" >
-                Remove
-                </Button>{" "}</div>
-
-                <div className="text-wrapper-9"> <Button className="butn" variant="primary" >
-                Remove
-                </Button>{" "}</div>
-
-                <div className="text-wrapper-10"> <Button className="butn" variant="primary" >
-                Remove
-                </Button>{" "}</div>
-
-                <div className="rectangle-8" />
-
-                <div className="rectangle-9" />
-
-                <div className="rectangle-10" />
-
-                <div className="rectangle-11" />
-
-                <div className="rectangle-12" />
-
-                <div className="rectangle-13" />
-
-                <div className="text-wrapper-11">
-                    View experts
                 </div>
-
-                <div className="text-wrapper-12">A.B.C. Silva</div>
-
-                <div className="text-wrapper-13">A.B.C. Silva</div>
-
-                <div className="text-wrapper-14">A.B.C. Silva</div> 
-                <div className="text-wrapper-15">A.B.C. Silva</div>
-
-                <div className="text-wrapper-16">A.B.C. Silva</div>
-
-                <div className="add-users"><Button className="butn" variant="primary" >
-                Add experts
-                </Button>{" "}</div>
-
-                </div>
-
+              ))}
             </div>
+          </div>
 
-            </div>
-);
-};
-export default AdminEx
+          {/* //******************* */}
+
+          {/* <div className="text-wrapper-6">
+            <Button className="butn" variant="primary">
+              Remove
+            </Button>{" "}
+          </div>
+
+          <div className="text-wrapper-7">
+            <Button className="butn" variant="primary">
+              Remove
+            </Button>{" "}
+          </div>
+
+          <div className="text-wrapper-8">
+            {" "}
+            <Button className="butn" variant="primary">
+              Remove
+            </Button>{" "}
+          </div>
+
+          <div className="text-wrapper-9">
+            {" "}
+            <Button className="butn" variant="primary">
+              Remove
+            </Button>{" "}
+          </div>
+
+          <div className="text-wrapper-10">
+            {" "}
+            <Button className="butn" variant="primary">
+              Remove
+            </Button>{" "}
+          </div> */}
+
+          {/* <div className="rectangle-8" />
+
+          <div className="rectangle-9" />
+
+          <div className="rectangle-10" />
+
+          <div className="rectangle-11" />
+
+          <div className="rectangle-12" />
+
+          <div className="rectangle-13" /> */}
+
+          <div className="text-wrapper-11">View experts</div>
+
+          {/* <div className="text-wrapper-12">A.B.C. Silva</div>
+
+          <div className="text-wrapper-13">A.B.C. Silva</div>
+
+          <div className="text-wrapper-14">A.B.C. Silva</div>
+          <div className="text-wrapper-15">A.B.C. Silva</div>
+
+          <div className="text-wrapper-16">A.B.C. Silva</div> */}
+
+          <div className="add-users">
+            <Button className="butn" variant="primary">
+              Add experts
+            </Button>{" "}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AdminEx;
