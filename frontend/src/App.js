cac88c108a0e61641eb084e8cd2f97a5137f7042
Pamudi Srikanthi <95670474+PamudiSrikanthi@users.noreply.github.com>
2024-03-07 20:15:57 +0530

    Create admin Add_cousesCerificates page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,6 +23,8 @@ import Machine_Learning from './pages/Machine_Learning'
 import Mobile_app_Dev from './pages/Mobile_app_Dev'
 import Web_Development from './pages/Web_Development'
 
+import Add_cousesCerificates from './pages/Admin/Add_cousesCerificates/Add_cousesCerificates'
+
 
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 
@@ -54,6 +56,7 @@ function App() {
           <Route path="/Mobile_app_Dev" element={<Mobile_app_Dev></Mobile_app_Dev>} ></Route>
           <Route path="/Web_Development" element={<Web_Development></Web_Development>} ></Route>
 
+          <Route path="/Add_cousesCerificates" element={<Add_cousesCerificates></Add_cousesCerificates>} ></Route>
         </Routes>
         
       </BrowserRouter>
---
+++ frontend/src/pages/Admin/Add_cousesCerificates/Add_cousesCerificates.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Add_cousesCerificates() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Add_cousesCerificates
+
