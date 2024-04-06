acc7a685fb8f30e5c81b06bd756eb12e0fa94cde
Sahan251 <sahanudayanga64@gmail.com>
2024-04-06 12:06:19 +0530

    about us update

--- frontend/src/pages/AbtUs/Det/AboutPage.jsx
+++ frontend/src/pages/AbtUs/Det/AboutPage.jsx
@@ -7,9 +7,9 @@ function AboutPage() {
     <div>
       <NavBar />
       <div className="abt-container">
-        <h1>About Us</h1>
         <div className="abt-content">
-          <img src={logo} alt="Abt Us" className="abt-image" />
+        <h1>About Us</h1>
+          {/* <img src={logo} alt="Abt Us" className="abt-image" /> */}
           <p className="abt-text">
            The mission of supporting the career development of IT undergraduates through industry experts is to bridge the gap between 
            academic learning and real-world industry requirements, thereby empowering students to succeed in their IT careers. 
@@ -19,8 +19,14 @@ function AboutPage() {
            Undergraduates can seek assistance and advice from these experts, helping them overcome challenges and make informed decisions about their career paths.
             <br />
             <br />
-           
+            The mission of supporting the career development of IT undergraduates through industry experts is to bridge the gap between 
+           academic learning and real-world industry requirements, thereby empowering students to succeed in their IT careers. 
+           The primary focus is to provide students with practical insights, industry knowledge, and mentorship to enhance their skills, confidence, and employability
+           in the IT sector Guidance and support are paramount,
+           and the program provides clear instructions for identifying and contacting suitable tech experts. 
+           Undergraduates can seek assistance and advice from these experts, helping them overcome challenges and make informed decisions about their career paths.
           </p>
+
         </div>
       </div>
     </div>
